d69f0df181bf49cdbf4e17e9edcd5c158354d5f9
ahmed nasser <ahmed.timo189@gmail.com>
2023-02-26 14:19:45 +0200

    init app with first server run and setup ejs file

---
+++ Crud_app/Sever.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv');
+dotenv.config({path:'/Crud_app/config.env'})
+const PORT = 3000;
+const morgan = require('morgan')
+const bodyparser=require('body-parser')
+const path=require("path")
+
+
+app.get("/",(req,res)=>{
+    res.render('index.ejs');
+})
+
+// log request
+app.use(morgan('tiny'))
+
+// body parser 
+app.use(bodyparser.urlencoded({extended:true}))
+
+// set view engine 
+app.set("view engine","ejs")
+
+// load assets 
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+
+
+
+app.listen(PORT,()=>{console.log(`the app is running at http://localhost:${PORT}`)})
